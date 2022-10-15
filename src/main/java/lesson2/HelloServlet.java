e65f501fa86a032df46fe01be83451c2f2ba5dc0
IzzatRamazanli <izzat.ramazanli@gmail.com>
2022-10-15 13:59:43 +0400

    lesson2

--- src/main/java/lesson2/HelloServlet.java
+++ src/main/java/lesson2/HelloServlet.java
@@ -1,19 +1,31 @@
 package lesson2;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class HelloServlet extends HttpServlet {
-    /*http://localhost:8080/hello?x=5&y=80*/
+    /*http://localhost:8080/hello*/
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        //this is hard coding
+        String badExample = "src/main/java/lesson2/test.html";
+
+        //must create resources directory under main folder
+        String file = getClass().getClassLoader().getResource("test.html").getFile();
+
+
+        List<String> lines = Files.readAllLines(Path.of(file));
         try (PrintWriter writer = resp.getWriter()) {
-            writer.println("Hello");
+            for (String line : lines) {
+                writer.println(line);
+            }
         }
     }
 
