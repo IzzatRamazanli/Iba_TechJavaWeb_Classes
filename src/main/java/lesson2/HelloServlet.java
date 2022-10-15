8201cff9a7f0dc5e8bf0babf95fcc0221371c496
IzzatRamazanli <izzat.ramazanli@gmail.com>
2022-10-15 14:19:59 +0400

    lesson2errorstill

--- src/main/java/lesson2/HelloServlet.java
+++ src/main/java/lesson2/HelloServlet.java
@@ -3,8 +3,7 @@ package lesson2;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
@@ -15,13 +14,13 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         //this is hard coding
-        String badExample = "src/main/java/lesson2/test.html";
+        String badExample = "src/main/resources/test2.html";
 
         //must create resources directory under main folder
-        String file = getClass().getClassLoader().getResource("test.html").getFile();
+        String fileName = this.getClass().getClassLoader().getResource("test2.html").getFile();
 
 
-        List<String> lines = Files.readAllLines(Path.of(file));
+        List<String> lines = Files.readAllLines(Path.of(badExample));
         try (PrintWriter writer = resp.getWriter()) {
             for (String line : lines) {
                 writer.println(line);
