77b7e28b01d0eb272756ae3ff897d6a8c6c55e0f
IzzatRamazanli <izzat.ramazanli@gmail.com>
2022-10-15 15:06:45 +0400

    lesson2errorsolved
    https://stackoverflow.com/questions/15749192/how-do-i-load-a-file-from-resource-folder

--- src/main/java/lesson2/HelloServlet.java
+++ src/main/java/lesson2/HelloServlet.java
@@ -4,9 +4,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.Objects;
 
 public class HelloServlet extends HttpServlet {
     /*http://localhost:8080/hello*/
@@ -14,13 +17,18 @@ public class HelloServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 
         //this is hard coding
-        String badExample = "src/main/resources/test2.html";
+        String badExample = "src/main/resources/test.html";
 
         //must create resources directory under main folder
-        String fileName = this.getClass().getClassLoader().getResource("test2.html").getFile();
+        URI uri = null;
+        try {
+            uri = Objects.requireNonNull(this.getClass().getClassLoader().getResource("test.html")).toURI();
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
 
 
-        List<String> lines = Files.readAllLines(Path.of(badExample));
+        List<String> lines = Files.readAllLines(Path.of(uri));
         try (PrintWriter writer = resp.getWriter()) {
             for (String line : lines) {
                 writer.println(line);
