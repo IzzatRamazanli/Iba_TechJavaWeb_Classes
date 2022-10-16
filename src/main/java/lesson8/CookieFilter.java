183cdd6d55858a8a782216d980df80899c6ce4c6
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-10-17 02:07:39 +0400

    Filter API

--- src/main/java/lesson8/CookieFilter.java
+++ src/main/java/lesson8/CookieFilter.java
@@ -11,13 +11,13 @@ public class CookieFilter implements HttpFilter {
     private boolean isCookiePresent(HttpServletRequest request) {
         return SessionRelated.find(request).isPresent();
     }
+
     @Override
     public void doHttpFilter(HttpServletRequest request,
                              HttpServletResponse response,
                              FilterChain chain) throws IOException, ServletException {
         if (isCookiePresent(request)) chain.doFilter(request, response);
-        else {
-            response.sendRedirect("/login");
-        }
+        else response.sendRedirect("/login");
+
     }
 }
