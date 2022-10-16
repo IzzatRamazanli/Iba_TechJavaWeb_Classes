47b60561936afabf092cbf9688a876b69f1bfb8b
IzzatRamazanli <86556311+IzzatRamazanli@users.noreply.github.com>
2022-10-16 18:07:50 +0400

    servlet-intro

--- src/main/java/lesson7/CalcServlet.java
+++ src/main/java/lesson7/CalcServlet.java
@@ -1,18 +1,43 @@
 package lesson7;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
+import java.util.UUID;
 
 public class CalcServlet extends HttpServlet {
-    /*http://localhost:8080/calc*/
+    /*http://localhost:8080/calc?x=5&y=7*/
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Cookie[] cookies = req.getCookies();
+        Optional<String> found = Optional.empty();
+
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals(SessionRelated.COOKIE_NAME)) {
+                    found = Optional.of(cookie.getValue());
+                }
+            }
+        }
 
         try (PrintWriter writer = resp.getWriter()) {
-            writer.println("calculator");
+
+            writer.printf("user id is : %s\n", found.orElse("not found\n"));
+
+            String xs = req.getParameter("x");
+            String ys = req.getParameter("y");
+            int x = Integer.parseInt(xs);
+            int y = Integer.parseInt(ys);
+            int z = x + y;
+
+            writer.printf("adding... %d + %d = %d", x, y, z);
+        }
+        if (found.isEmpty()) {
+            resp.addCookie(new Cookie(SessionRelated.COOKIE_NAME, UUID.randomUUID().toString()));
         }
     }
 }
---
+++ src/main/java/lesson7/SessionRelated.java
@@ -0,0 +1,5 @@
+package lesson7;
+
+public class SessionRelated {
+    public static final String COOKIE_NAME = "user_id";
+}
